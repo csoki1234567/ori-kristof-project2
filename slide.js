51625d84d3a9923028aa67526bf586f585355f90
KristofUwU <balogh.kristof.janos2009@tanulo.boronkay.hu>
2025-04-24 13:59:09 +0200

    mentés

--- slide.js
+++ slide.js
@@ -1,21 +1,3 @@
 <script>
-let currentIndex = 0;
-  const slides = document.querySelectorAll('.books');
 
-  function showSlide(index) {
-    slides.forEach(slide => slide.classList.remove('active'));
-    slides[index].classList.add('active');
-  }
-
-  function nextSlide() {
-    currentIndex = (currentIndex + 1) % slides.length;
-    showSlide(currentIndex);
-  }
-
-  function prevSlide() {
-    currentIndex = (currentIndex - 1 + slides.length) % slides.length;
-    showSlide(currentIndex);
-  }
-
-  showSlide(currentIndex);
 </script>
