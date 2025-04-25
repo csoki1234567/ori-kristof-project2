14a95187c254e13f5863dfac6e042eced38a9eef
KristofUwU <balogh.kristof.janos2009@tanulo.boronkay.hu>
2025-04-25 16:07:02 +0200

    kész van a mobilnézethez a javascript és működik is \(^_^)/

--- slide.js
+++ slide.js
@@ -3,16 +3,16 @@ const buttons = document.querySelectorAll("[data-carousel-button]");
 
 buttons.forEach(button => {
   button.addEventListener("click", () => {
-    const offset = button.dataset.carouselButton === "next" ? 1 : -1; // Ha next, 1, ha prev, -1
-    const slides = button.closest("[data-carousel]").querySelector("[data-slides]"); // Melyik karuszel
-    const activeSlide = slides.querySelector(".active"); // Az épp aktív doboz
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1; 
+    const slides = button.closest("[data-carousel]").querySelector("[data-slides]"); 
+    const activeSlide = slides.querySelector(".active"); 
 
-    let newIndex = [...slides.children].indexOf(activeSlide) + offset; // Új index számítása
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset; 
 
-    if (newIndex < 0) newIndex = slides.children.length - 1; // Ha az első elem előtt vagyunk, az utolsóhoz ugrik
-    if (newIndex >= slides.children.length) newIndex = 0; // Ha az utolsó elem után vagyunk, az elsőhöz ugrik
+    if (newIndex < 0) newIndex = slides.children.length - 1; 
+    if (newIndex >= slides.children.length) newIndex = 0; 
 
-    slides.children[newIndex].classList.add("active"); // Új elem aktiválása
-    activeSlide.classList.remove("active"); // Régi elem eltávolítása
+    slides.children[newIndex].classList.add("active"); 
+    activeSlide.classList.remove("active"); 
   });
 });
