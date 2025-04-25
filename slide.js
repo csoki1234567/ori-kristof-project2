1027aa2fe4eccf0bc6fbe2941b162b6213094b67
KristofUwU <balogh.kristof.janos2009@tanulo.boronkay.hu>
2025-04-25 12:09:04 +0200

    javascript nem működik, elkezdem csinálni a gépes nézetett

--- slide.js
+++ slide.js
@@ -1,18 +1,18 @@
-const buttons = document.querySelector("[data-carousel-button]")
+
+const buttons = document.querySelectorAll("[data-carousel-button]");
 
 buttons.forEach(button => {
-    button.addEventListener("click", () => {
-        const offset = button.dataset.carouselButton === "next" ? 1: -1
-        const slides = button
-        .closest("[data-carousel]")
-        .querySelector("[data-slides]")
+  button.addEventListener("click", () => {
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1; // Ha next, 1, ha prev, -1
+    const slides = button.closest("[data-carousel]").querySelector("[data-slides]"); // Melyik karuszel
+    const activeSlide = slides.querySelector(".active"); // Az épp aktív doboz
 
-        const activeSlide = slides.querySelector("[data-active]")
-        let newIndex = [...slides.children].indexOf(activeSlide) + offset
-        if (newIndex < 0 ) newIndex = slides.children.length - 1
-        if (newIndex >= slides.children.length) newIndex = 0
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset; // Új index számítása
 
-        slides.children[newIndex].dataset.active = true 
-        delete activeSlide.dataset.active
-    })
-})
+    if (newIndex < 0) newIndex = slides.children.length - 1; // Ha az első elem előtt vagyunk, az utolsóhoz ugrik
+    if (newIndex >= slides.children.length) newIndex = 0; // Ha az utolsó elem után vagyunk, az elsőhöz ugrik
+
+    slides.children[newIndex].classList.add("active"); // Új elem aktiválása
+    activeSlide.classList.remove("active"); // Régi elem eltávolítása
+  });
+});
